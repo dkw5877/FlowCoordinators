21285685afbfa4aa1cb645e6dc68d9f4b6f25303
dennis-walsh <intrnetmail@yahoo.com>
2019-04-16 08:34:59 -0700

    resolve merge conflict

--- FlowCoordinators/Coordinators/AppCoordinator.swift
+++ FlowCoordinators/Coordinators/AppCoordinator.swift
@@ -55,19 +55,9 @@ extension AppCoordinator : AuthenticationCoordinatorDelegate {
         showProfile()
     }
 
-    //we need a better way to find coordinators
-    fileprivate func removeCoordinator(coordinator: Coordinator) {
-
-        var idx: Int?
-        for (index,value) in childCoordinators.enumerated() where value === coordinator {
-                idx = index
-                break
-        }
-
-        if let index = idx {
-            childCoordinators.remove(at: index)
-        }
-
+    fileprivate func removeCoordinator(coordinator:Coordinator) {
+        let arr = childCoordinators.filter {$0 !== coordinator}
+        childCoordinators = arr
     }
 }
 
